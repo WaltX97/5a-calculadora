bac234fbea38d457c0090a5c5946e7cd43cee47c
Walter Monteiro <aluno20187@ipt.pt>
2019-03-14 16:14:33 +0000

    criação da interface gráfica inicial da calculadora

---
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculadora.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }  
+        // GET: Post
+        [HttpPost]
+        public ActionResult Index(string btn,string visor){
+            return View();
+        }
+    }
+}
